3ad877f15e4060e6ebd61e325bbd25e497c7d6ca
SirJohnSem <jewgienijdanilczik@mail.ru>
2021-08-18 10:01:23 +0200

    add menu burger

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const menuBurger = document.querySelector('.menu__icon');
 const menuBody = document.querySelector('.menu__body');
+const iconMenu = document.querySelector('.icon-menu');
+
 menuBurger.addEventListener('click', function(){
     menuBody.classList.add('_active');
-    return menuBody;
+    iconMenu.classList.add('_active');
+    return menuBody, iconMenu;
 })
+
+/*iconMenu.addEventListener('click', function(){
+    menuBody.classList.remove('_active');
+    iconMenu.classList.remove('_active');
+    return menuBody, iconMenu;
+})*/
