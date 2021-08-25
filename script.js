395656d42ff40601e04810f463d6955266ed6240
SirJohnSem <jewgienijdanilczik@mail.ru>
2021-08-25 11:39:48 +0200

    fix styles

--- script.js
+++ script.js
@@ -3,13 +3,8 @@ const menuBody = document.querySelector('.menu__body');
 const iconMenu = document.querySelector('.icon-menu');
 
 menuBurger.addEventListener('click', function(){
-    menuBody.classList.add('_active');
-    iconMenu.classList.add('_active');
-    return menuBody, iconMenu;
+    menuBody.classList.toggle('_active');
+    iconMenu.classList.toggle('_active');
 })
 
-/*iconMenu.addEventListener('click', function(){
-    menuBody.classList.remove('_active');
-    iconMenu.classList.remove('_active');
-    return menuBody, iconMenu;
-})*/
+
